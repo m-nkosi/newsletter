0d23da1c2099222d768d11945e91ba5fb6e2f906
32431430 <mxolisinkosi2360@gmail.com>
2024-02-19 07:29:43 +0200

    added the sucess page for mobile

--- javaScript/index.js
+++ javaScript/index.js
@@ -5,11 +5,26 @@ $(document).ready(() => {
     // javaScript check email
     let email = document.getElementById("email");
     $form.on("submit",() => {
-        if (!email.validity.valid){
+        if (!email.validity.valid || email.value ==='' || email.value === null ){
             $errorLabel.show();
             $email.addClass('errorInput');
             return false;
+        }else{
+            $("#container").fadeOut();
+            $("#successSection").css("display", "flex").fadeIn("slow");
+            if ($(".errorInput")){
+                $errorLabel.hide();
+                $email.removeClass('errorInput');
+            }
+            return false;
         }
+    })
+    $("#buttonForSuccessPage").on("click", () => {
+        $("#successSection").fadeOut();
+        document.querySelector("#email").value = "";
+        $("#container").fadeIn("slow");
+
+        return true;
     })
 
 });
